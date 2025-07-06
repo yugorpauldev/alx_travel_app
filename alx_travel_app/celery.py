7156f1001b4b1cf20b8b6bfd95bc24ac7b6654a7
yugorpaul <nhantumboyugor@gmail.com>
2025-07-06 20:55:13 +0200

    re change files

--- alx_travel_app/celery.py
+++
@@ -1,8 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alx_travel_app.settings')
-
-app = Celery('alx_travel_app')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
